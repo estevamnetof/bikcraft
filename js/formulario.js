0e76fafc6de2d98750c6429ad3aa32715a22f0f8
Estevam Neto <estevam.neto07@gmail.com>
2023-02-25 00:54:19 -0300

    formulario javascript

---
+++ js/formulario.js
@@ -0,0 +1,25 @@
+const formulario = document.querySelector('form');;
+
+function formularioEnviado(resposta) {
+    if(resposta.ok) {
+        '<p class="font-2-l" style="grid-column: 1/-1; padding: 1rem; border-radius: 4px; background: #f7f7f7"><span style="color:#317A00">Mensagem enviada</span>, em breve entraremos em contato. Geralmente respondemos em até 24h</p>'
+    } else {
+        formulario.innerHTML = '<p class="font-2-l" style="grid-column: 1/-1; padding: 1rem; border-radius: 4px; background: #f7f7f7"><span style="color: #E00000">Erro no envio</span>, você pode enviar diretamente diretamente para o nosso email em contato@bikcraft.net</p>'
+    }
+}
+
+function enviarFormulario(event) {
+    event.preventDefault();
+    const botao = document.querySelector('form button');
+    botao.disabled = true;
+    botao.innerText = "Enviando...";
+
+    const data = new FormData(formulario);
+
+    fetch('./enviar.php', {
+        method: 'POST',
+        body: data
+    }).then(formularioEnviado);
+}
+
+formulario.addEventListener('submit', enviarFormulario);
